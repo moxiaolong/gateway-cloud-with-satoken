0a2b8e7d866ac9cb11cd301494401fb40589f924
dragon <moxiaolong950811>
2022-01-28 14:31:28 +0800

    调用权限服务

--- src/main/java/com/twwg/gateway/GateWayApplication.java
+++ src/main/java/com/twwg/gateway/GateWayApplication.java
@@ -1,8 +1,10 @@
 package com.twwg.gateway;
 
+import com.twwg.common.core.EnableMicroServiceCommonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 网关应用程序
@@ -12,6 +14,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableMicroServiceCommonConfig
+@EnableFeignClients("com.twwg.api")
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
--- src/main/java/com/twwg/gateway/fliter/AuthFilter.java
+++ src/main/java/com/twwg/gateway/fliter/AuthFilter.java
@@ -1,6 +1,8 @@
 package com.twwg.gateway.fliter;
 
+import com.twwg.api.SecurityApiFeign;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -11,6 +13,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * 日志过滤器
  *
@@ -20,19 +26,46 @@ import reactor.core.publisher.Mono;
 @Component
 @Slf4j
 public class AuthFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    SecurityApiFeign securityApiFeign;
+
+    List<String> allowUri = new ArrayList<>();
+
+    AuthFilter() {
+        allowUri.add("/security/captcha");
+        allowUri.add("/security/noCaptchaLogin");
+        allowUri.add("/security/login");
+        allowUri.add("/security/appLogin");
+        allowUri.add("/security/logout");
+    }
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
         String path = exchange.getRequest().getURI().getPath();
-        log.info(path);
+
         //假装如果没有权限 就响应无权限 http://localhost:8888/api/your-service-name/user/ban
-        if (path.startsWith("/your-service-name/user/ban")){
-            DataBuffer bodyDataBuffer = response.bufferFactory().wrap("无权限".getBytes());
-            response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
-            return response.writeWith(Mono.just(bodyDataBuffer));
+        for (String aAllowUri : allowUri) {
+            if (path.startsWith(aAllowUri)) {
+                return chain.filter(exchange);
+            }
         }
-        return chain.filter(exchange);
+        List<String> tokens = request.getHeaders().get("token");
+        if (tokens!=null&&tokens.size()>0){
+            String token = tokens.get(0);
+            if (token!=null&&!token.isEmpty()){
+                Set<String> permission = securityApiFeign.doGetAuthorizationInfo();
+
+
+            }
+        }
+
+        DataBuffer bodyDataBuffer = response.bufferFactory().wrap("无权限".getBytes());
+        response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
+        return response.writeWith(Mono.just(bodyDataBuffer));
+
     }
 
     @Override
