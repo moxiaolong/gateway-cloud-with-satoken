7bc88b0377b3f275cbf3021e1144c990105dce8f
dragon <moxiaolong950811>
2022-03-01 18:53:47 +0800

    对接新的鉴权服务，增加token解析出userID，放入请求header

--- src/main/java/com/twwg/gateway/fliter/AuthFilter.java
+++ src/main/java/com/twwg/gateway/fliter/AuthFilter.java
@@ -1,6 +1,7 @@
 package com.twwg.gateway.fliter;
 
-import com.twwg.gateway.feign.SecurityApiFeign;
+import cn.hutool.jwt.JWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -16,7 +17,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -31,18 +32,16 @@ import java.util.stream.Collectors;
 public class AuthFilter implements GlobalFilter, Ordered {
 
     @Autowired
-    SecurityApiFeign securityApiFeign;
-    @Autowired
     private WebClient.Builder webClientBuilder;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private final JWT jwt = JWT.create();
 
     List<String> allowUri = new ArrayList<>();
 
     AuthFilter() {
-        allowUri.add("/security/captcha");
-        allowUri.add("/security/noCaptchaLogin");
-        allowUri.add("/security/login");
-        allowUri.add("/security/appLogin");
-        allowUri.add("/security/logout");
+        allowUri.add("/login/captcha");
+        allowUri.add("/login");
     }
 
     @Override
@@ -57,16 +56,26 @@ public class AuthFilter implements GlobalFilter, Ordered {
                 return chain.filter(exchange);
             }
         }
+        //token校验
         List<String> tokens = request.getHeaders().get("token");
         if (tokens != null && tokens.size() > 0) {
             String token = tokens.get(0);
             if (token != null && !token.isEmpty()) {
-                return webClientBuilder.build().post().uri("lb://security/security/security/doGetAuthorizationInfo").header("token", token).retrieve().bodyToMono(Set.class).flatMap(authSet -> {
-                    if (checkPermission(authSet, path, request.getMethodValue())) {
-                        return chain.filter(exchange);
+                Mono<Void> mono = webClientBuilder.build().get().uri("lb://iot-cloud-auth/auth/pathList").header("token", token).retrieve().bodyToMono(Map.class).flatMap(map -> {
+                    ArrayList<Map<String, String>> dataString = (ArrayList) map.get("data");
+                    if (checkPermission(dataString, path, request.getMethodValue())) {
+                        //过了这里 说明token是有效的 解析id 放入请求头给服务使用
+                        String id = jwt.parse(token).getPayload("id").toString();
+                        ServerHttpRequest newReq = request.mutate().header("userId", id).build();
+                        return chain.filter(exchange.mutate().request(newReq).build());
                     }
                     response.setStatusCode(HttpStatus.UNAUTHORIZED);
                     return exchange.getResponse().setComplete();
+                });
+                return mono.doOnError((throwable) -> {
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    String message = throwable.getMessage();
+                    log.error(message);
 
                 });
             }
@@ -81,23 +90,19 @@ public class AuthFilter implements GlobalFilter, Ordered {
         return Ordered.LOWEST_PRECEDENCE;
     }
 
-    private boolean checkPermission(Set<String> permission, String path, String method) {
-        List<String> permissionList = permission.stream()
-                //过滤method
-                .filter(t -> t.startsWith(method) || t.startsWith("*"))
-                //截取url部分  POST:/auth/isLogin -> /auth/isLogin
-                .map(t -> {
-                    if (t.startsWith("*")) {
-                        return t.substring(2);
-                    }
-                    return t.substring(method.length() + 1);
-                }).collect(Collectors.toList());
-
-        for (String aPermission : permissionList) {
-            if (Pattern.matches(aPermission, path)) {
+    private boolean checkPermission(List<Map<String, String>> authPathList, String path, String method) {
+        List<Map<String, String>> collect = authPathList.stream().filter(authPath -> authPath.get("method").equals(method) || "*".equals(authPath.get("method"))).collect(Collectors.toList());
+        if (collect.size() == 0) {
+            return false;
+        }
+        for (Map<String, String> map : collect) {
+            String uri = map.get("uri");
+            uri = uri.replace("*", ".*");
+            if (Pattern.matches(uri, path)) {
                 return true;
             }
         }
+
         return false;
     }
 }
